74d2d0a5b10375035efbb55f733d5062362525f5
Sameer Verma <na21b061@smail.iitm.ac.in>
2025-11-30 23:27:06 +0530

    feat: add Google site verification to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,6 +72,9 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+  verification: {
+    google: "NAlIH9k-f2Xwk4cvXUpEw3hsL9a56pR_2X0ZBdBKwQ4",
+  },
 };
 
 export default function RootLayout({
